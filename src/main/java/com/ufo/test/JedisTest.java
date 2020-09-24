58bedb396ead5e40ed37570af7448bea837f6a5c
huangdongyang <757551888@qq.com>
2020-09-24 15:31:07 +0800

    111

---
+++ src/main/java/com/ufo/test/JedisTest.java
@@ -0,0 +1,35 @@
+package com.ufo.test;
+
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * projectName: jedis
+ * author:小黄同学
+ * time: 2020/9/24 14:15
+ * description:
+ */
+public class JedisTest {
+    @Test
+    public void test(){
+        //连上redis数据库
+        Jedis jedis = new Jedis("47.99.223.210",6379);
+        jedis.set("username","jack");
+        String username = jedis.get("username");
+        System.out.println("username = " + username);
+    }
+    @Test
+    public void test1() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(50);
+
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig,"47.99.223.210",6379);
+        Jedis jedis = jedisPool.getResource();
+        User user = new User(1001,"zhangsan");
+        jedis.set("user", JSON.toJSONString(user));
+        System.out.println(jedis.get("user"));
+    }
+}
